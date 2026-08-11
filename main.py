04e7e39d2dbd1053ad14b7fe1de6939c05e38c52
Raha Al-Gethami <raha.algethami@gmail.com>
2026-08-11 08:37:43 +0300

    Refactor QR code generation using QRuser class
    Refactor QR code generation to use QRuser class and streamline the process.

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import input
-import QR_generator
+from QRuser import QRuser
 import os
 
 
 data_list = input.getData()
-
 os.makedirs("output", exist_ok=True)
+
+user = QRuser("name")
+
+user.Make_qr("cisco.com", "cisco")
+user.Make_qr("iherb.com", "iherb")
+
 for line in data_list:
-    line = line.strip()  # strips the \n character
-    strings = line.split(",")
-    name = strings[0]
-    url = strings[1]
-
-    file_name = f"output/{name}_QR.png"
-
-    QR_generator.Make_qr(url, file_name)
+    name, url = line.strip().split(",")
+    user.Make_qr(url.strip(), name.strip())
+user.summary()
